d992222b59d1387d03ac9e80973f09e08ca90a06
Taras <taras.shutskyi98@gmail.com>
2021-07-11 00:01:36 +0300

    Add Bootstrap, Style, Images on Event app

--- events/views.py
+++ events/views.py
@@ -1,3 +1,10 @@
+from .models import Event
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def home(request):
+    events = Event.objects
+    return render(request, "events/home.html" , {'events':events})
