6145abd8a287178341161f980d902679b81e5367
Taras <taras.shutskyi98@gmail.com>
2021-07-11 18:23:05 +0300

    static files and function site

--- blog/models.py
+++ blog/models.py
@@ -11,3 +11,6 @@ class Post(models.Model):
 
     def get_summary(self):
         return self.text[:70]
+
+    def __str__(self):
+        return self.title
--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -123,10 +123,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+STATICFILES_DIRS = [ 
+    BASE_DIR / 'myawesomeblog/static/'
+    
+]
+
+STATIC_ROOT =  BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
-MEDIA_ROOT =  BASE_DIR / 'media'
 
+MEDIA_ROOT =  BASE_DIR / 'media'
 MEDIA_URL = "/awesome_media/"
 
 
